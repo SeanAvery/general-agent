c4638307444e3d55d7f0a8dcda06e4461415ac84
Sean Pollock <seanpollock3344@gmail.com>
2018-04-06 13:24:16 -0400

    getting actin and observation sizes

--- Game.py
+++ Game.py
@@ -1,7 +1,21 @@
 import retro
 
+class Game():
+    def __init__(self):
+        self.env = retro.make(game='Airstriker-Genesis', state='Level1')
+        self.get_action_dim()
+        self.get_obs_dim()
+
+    def get_action_dim(self):
+        self.action_size = self.env.action_space.n
+        print('self.action_size', self.action_size)
+
+    def get_obs_dim(self):
+        self.obs_size = len(self.senv.observation_space.low)
+        print('self.obs_size', self.obs_size)
+
+
 if __name__ == '__main__':
-    env = retro.make(game='Airstriker-Genesis', state='Level1')
     obs = env.reset()
     while True:
         obs, rew, done, info = env.step(env.action_space.sample())
