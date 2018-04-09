c428b7a312f5c6978324508308f9315bc2bf1424
Sean Pollock <seanpollock3344@gmail.com>
2018-04-08 21:17:50 -0400

    colored images: 224, 320, 3)

--- Game.py
+++ Game.py
@@ -1,29 +1,42 @@
 import retro
+import numpy as np
 
 class Game():
     def __init__(self):
         self.env = retro.make(game='Airstriker-Genesis', state='Level1')
         self.get_action_dim()
         self.get_obs_dim()
-
+    
+    '''
+        SETUP
+        action_size: [Int]
+        obs_size: Numpy np.ndarray
+        action_size: Array
+    ''' 
     def get_action_dim(self):
-        self.action_size = self.env.action_space.n
-        print('self.action_size', self.action_size)
+        self.action_size = int(self.env.action_space.n)
+        print('action_size', self.action_size, type(self.action_size))
 
     def get_obs_dim(self):
-        self.obs_size = len(self.env.observation_space.low)
-        print('self.obs_size', self.obs_size)
+        self.obs_size = self.env.observation_space.low.shape
+        print('obs_size', self.obs_size, type(self.obs_size))
+    
+    '''
+        UTILS
+    '''
 
     def reshape(self, vector):
         return vector.reshape(1, self.obs_size) 
-    
+
+    '''
+        SIMULATION
+    '''
     def run_simulation(self, num_episodes):
         for i in range(num_episodes):
             self.run_episode()
     
-    def run_episodes(self):
-        self.old_obs = self.reshape(self.env.reset())
-        
+    def run_episode(self):
+        self.old_obs = self.env.reset()
         while True:
             done = self.run_tick()
             if done:
@@ -32,8 +45,8 @@ class Game():
     def run_tick(self):
         action = self.env.action_space.sample()
         obs, reward, done, info = self.env.step(action)
+        print("reward", reward) 
         return done
 
 if __name__ == '__main__':
     game = Game()
-    
