ab2efefb57222d68320871dfc29a764c1582a1c3
Sean Pollock <seanpollock3344@gmail.com>
2018-04-09 12:32:51 -0400

    create conv and pool layer fns

--- Models/ConvNet1.py
+++ Models/ConvNet1.py
@@ -19,47 +19,32 @@ class ConvNet1():
         MODEL
     '''
     
-    def create_conv_layer(self):
+    def create_conv_layer(self, inputs, num_filters, kernal_size):
         return tf.layers.conv2d(
             inputs=inputs,
-            filters=32,
-            kernal_size=[5,5],
+            filters=num_filters),
+            kernal_size=kernal_size,
             padding="same",
             activation=tf.nn.relu)
 
+    def create_pool_layer(self, inputs, pool_size, num_strides):
+        return tf.layers.max_pooling2d(
+            inputs=inputs,
+            pool_size=pool_size,
+            strides=num_strides)
+
     def build_model(self):
+        # -1 for dynamically sized batch
         input_layer = tf.reshape(features['x'], [-1, 224, 320, 3])
         
-        conv1 = tf.layers.conv2d(
-            inputs=inputs,
-            filters=32,
-            kernal_size=[5, 5],
-            padding='same',
-            activation=tf.nn.relu)
+        conv1 = self.create_conv_layer(input_layer, 128, [5, 5])
         
-        pool1 = tf.layers.max_pooling2d(
-            inputs=conv1, 
-            pool_size=[2, 2], 
-            strides=2)
+        pool1 = self.create_pool_layer(conv1, [2, 2], 2)
         
-        conv2 = tf.layers.conv2d(
-            inputs=pool1,
-            filters=64,
-            kernal_size=[5, 5],
-            padding='same',
-            activation=tf.nn.relu)
-
-        pool2 = tf.layers.max_pooling2d(
-            inputs=conv2, 
-            pool_size =[2, 2], 
-            strides=1)
+        conv2 = self.create_pool_layer(pool1, 256, [5, 5])
         
+        pool2 = self.create_pool_layer(conv2, [2, 2], 2)
         
-
-
-
-        
-
 if __name__ == '__main__':    
     convnet = ConvNet1()
     convnet.set_hyperparams(0.0001)
