5e21ae2ca4b55dcb47cfd643750052a76990611d
Sean Pollock <seanpollock3344@gmail.com>
2018-04-10 10:57:57 -0400

    removing old material

--- ConvNet.py
+++
@@ -1,9 +0,0 @@
-import keras
-from keras.models import Sequential
-from keras.layers import Conv2D, Dense
-from keras.optimizers import Adam
-
-class ConvNet():
-    def build_model(self, obs_size, action_size):
-        model = Sequential()
-        
